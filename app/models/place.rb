bbf4b8caba03456f38532a9a86a5c682dd15b7f2
Cole LeBoeuf <cleboeufwhs@gmail.com>
2019-09-12 07:16:19 +0000

    added comment section

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,8 +1,11 @@
 class Place < ApplicationRecord
   belongs_to :user
+  has_many :comments
+
   geocoded_by :address
   after_validation :geocode
   
-  validates :name, :address, :description, presence: true
+  validates :name, presence: true, length: { minimum: 3, too_short: "must be longer than %{count} characters." }
+  validates :address, :description, presence: true
 end
 
